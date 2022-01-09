49e55e27693803d60e334c52aa74526f58cbfc8a
SklifCybe <i.s.toaccept@gmail.com>
2022-01-09 12:40:33 +0300

    init: i did it, yup :)

--- src/index.js
+++ src/index.js
@@ -1,46 +1,104 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+const getArray = (expr) => {
+  return expr
+    .split('')
+    .map((el, id) => {
+      if (id % 10 === 0) {
+        return `., ${el}`;
+      }
+      return el;
+    })
+    .join('')
+    .split('., ')
+    .filter((el) => el !== '');
+};
+
+const deleteZeroes = (arr) => arr.map((el) => el.slice(el.indexOf('1')));
+
+const getArrNum = (arr) => {
+  return arr
+    .map((el) => {
+      return el
+        .split('')
+        .map((i, id) => {
+          if (id % 2 === 0) {
+            return `, ${i}`;
+          }
+          return i;
+        })
+        .join('');
+    })
+    .map((el) => {
+      return el.split(',');
+    })
+    .map((el) => {
+      return el.filter((i) => i !== '').map((i) => Number(i));
+    });
+};
+
+const transformToMorz = (arr) => {
+  return arr.map((el) => {
+    return el.map((i) => {
+      if (isNaN(i)) return 'space';
+      return i === 10 ? '.' : '-';
+    });
+  });
+};
+
+const decode = (expr) => {
+  const arr = transformToMorz(getArrNum(deleteZeroes(getArray(expr)))).map((el) => el.join(''));
+
+  const resultArr = arr.map((el) => {
+    return el === 'space' ? ' ' : MORSE_TABLE[el];
+  })
+
+  return resultArr.join('');
+};
+
+console.log(
+  decode(
+    '00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010',
+  ),
+);
 
 module.exports = {
-    decode
-}
+  decode,
+};
